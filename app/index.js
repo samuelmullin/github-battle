84cb563273e35b79d7393301fbc34b7a0e0adb1b
Sam Mullin <samuel.mullin@gmail.com>
2019-05-29 12:08:38 -0400

    Added battle functionality

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import Popular from './components/popular'
+import Battle from './components/battle'
 
 // Component: State + Lifecycle + UI
 class App extends React.Component {
@@ -9,7 +10,7 @@ class App extends React.Component {
   render(){
     return(
       <div className='container'>
-        <Popular />
+        <Battle />
       </div>
     )
   }
